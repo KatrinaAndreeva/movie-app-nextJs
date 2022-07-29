80b2e2610b1964489f46ca0bb5ae7cb784bec120
Kate <katya-andreeva@ukt.net>
2022-07-29 21:00:52 +0300

    "added navbar"

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,3 +1,19 @@
+import { useRouter } from 'next/router';
+import requests from '../utils/requests';
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  const router = useRouter();
+  return (
+    <div className="flex justify-center text-xl text-gray-200 bg-gray-600 select-none lg:text-2xl">
+      {Object.entries(requests).map(([key, { title, url }]) => (
+        <h2
+          onClick={() => router.push(`?genre=${key}`)}
+          className="m-6 cursor-pointer hover:text-white active:text-red-400"
+          key={key}
+        >
+          {title}
+        </h2>
+      ))}
+    </div>
+  );
 }
